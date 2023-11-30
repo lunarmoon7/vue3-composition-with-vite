75992aa9a68061e77092812f5fc928f0c0be4d34
lunarmoon7 <49crehbgr@gmail.com>
2023-11-30 14:53:50 +0900

    feat: html요소의 ref속성 추가하기

--- src/components/CompositionAPI.vue
+++ src/components/CompositionAPI.vue
@@ -1,31 +1,21 @@
 <script setup lang="ts">
-import { reactive, watch } from 'vue'
+import { ref, onMounted } from 'vue'
 
-const user = reactive({
-  name: 'coggie',
-  age: 18
+// ref에 타입을 전달할 때 제네릭을 사용한다.
+// null일 수도 있고, HTMLInputElement일 수도 있다.
+const inputEl = ref<HTMLInputElement | null>(null)
+
+onMounted(() => {
+  if (inputEl.value) {
+    // inputEl.value는 HTMLInputElement이다.
+    // ref의 타입 선언을 하지 않으면 에러가 발생한다.
+    inputEl.value.focus()
+  }
 })
-
-// 추가 설정이 없다면 객체는 참조로 전달된다. -> 감시하지 못한다.
-// 세 번째 인자로 deep: true를 전달하면 객체의 모든 속성을 감시한다.
-// 특정 속성의 변경만 감시하려면
-// 첫 번째 인자로 객체의 특정 속성을 반환하는 콜백함수를 전달하면 된다.
-
-// reactive로 선언한 반응형 데이터는 deep: true를 사용하지 않아도 된다.
-// 또한, 첫 번째 인자로 데이터 자체를 넘기면 된다.
-watch(user, (newVal) => {
-  console.log(newVal)
-})
-
-console.log(user.name, user.age)
 </script>
 
 <template>
-  <input
-    v-model="user.name"
-    type="text" />
-  <h1>{{ user.name }}</h1>
-  <h2></h2>
+  <input ref="inputEl" />
 </template>
 
 <style></style>
--- src/components/OptionsAPI.vue
+++ src/components/OptionsAPI.vue
@@ -1,9 +1,18 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-export default defineComponent({})
+export default defineComponent({
+  mounted() {
+    // HTML 요소에 연결된 ref는 this.$refs로 접근할 수 있습니다.
+    if (this.$refs.inputEl) {
+      ;(this.$refs.inputEl as HTMLInputElement).focus()
+    }
+  }
+})
 </script>
 
-<template></template>
+<template>
+  <input ref="inputEl" />
+</template>
 
 <style></style>
