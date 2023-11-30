544867326cf711844a544cd397980b9359606ca5
lunarmoon7 <49crehbgr@gmail.com>
2023-11-30 14:02:35 +0900

    feat: add computed

---
+++ src/components/CompositionAPI.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+const count = ref(0)
+
+const increase = () => {
+  count.value++
+}
+
+const double = computed(() => {
+  return count.value * 2
+})
+</script>
+
+<template>
+  <button @click="increase">Increase</button>
+  <h1>{{ count }}</h1>
+  <h2>{{ double }}</h2>
+</template>
---
+++ src/components/OptionsAPI.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  data() {
+    return {
+      count: 0
+    }
+  },
+  computed: {
+    double() {
+      return this.count * 2
+    }
+  },
+  methods: {
+    increase() {
+      this.count++
+    }
+  }
+})
+</script>
+
+<template>
+  <button @click="increase">Increase</button>
+  <h1>{{ count }}</h1>
+  <h2>{{ double }}</h2>
+</template>
